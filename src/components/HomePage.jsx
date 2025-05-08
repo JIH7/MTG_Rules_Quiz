6c06e728d19600bab73dc58c805d4d976b0b4bc7
Jeremy <jihlucky7@gmail.com>
2025-05-08 00:20:42 -0500

    CSS tweaks

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,7 @@
 const HomePage = ({ startQuiz, highScores }) => {
     return (
         <>
-            <h1>Magic: The Gathering Rules Quiz</h1>
+            <h1 className="home-header">Magic: The Gathering Rules Quiz</h1>
             <button onClick={startQuiz}>Start</button>
             <button onClick={highScores}>High Scores</button>
         </>
